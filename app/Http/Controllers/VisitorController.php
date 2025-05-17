2841cfad897a90b4c4e38849bde29ba4226f1c81
Nur Diantoro <nurdiantoro100@gmail.com>
2025-05-17 10:28:50 +0700

    1.17.1 Penambahan check 1 menit sebelum checkin

--- app/Http/Controllers/VisitorController.php
+++ app/Http/Controllers/VisitorController.php
@@ -19,8 +19,18 @@ class VisitorController extends Controller
     {
         $user = User::where('barcode', $request->barcode)->first();
 
+        // Cek Barcode bener apa engga
         if (!$user) {
             return redirect()->back()->with('barcode_not_exists', 'Barcode Not Found, Please Check Again.');
+        }
+
+        // Cek udah scan apa belom 1 menit terakhir
+        $recentCheckin = Visitor::where('barcode', $request->barcode)
+            ->where('created_at', '>=', now()->subMinute())
+            ->first();
+
+        if ($recentCheckin) {
+            return redirect()->back()->with('barcode_exists', 'Barcode Found.');
         }
 
         Visitor::create([
